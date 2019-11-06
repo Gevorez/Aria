55eb1dd6684c196617bdfb8f6805ff89e61be1c2
Dominik Rybicki <dominpl13@gmail.com>
2019-11-06 17:40:40 +0100

    da

--- wyklady/src/wyklady/rejestracja.java
+++ wyklady/src/wyklady/rejestracja.java
@@ -52,7 +52,7 @@ public class rejestracja extends JFrame {
 	 * Create the framee
 	 */
 	public rejestracja() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 396, 533);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
