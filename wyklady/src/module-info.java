6c6e19204321445429d7ad75797463d5d0973dc3
Dominik Rybicki <dominpl13@gmail.com>
2019-11-16 01:27:34 +0100

    usuwanie utworu

---
+++ wyklady/src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author domin
+ *
+ */
+module Aria {
+	exports wyklady;
+	exports testowe;
+
+	requires java.desktop;
+	requires java.sql;
+}
--- wyklady/src/wyklady/usuwanie_utworow.java
+++ wyklady/src/wyklady/usuwanie_utworow.java
@@ -12,17 +12,16 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.awt.event.ActionEvent;
 
-public class dodawnie_utworow {
+public class usuwanie_utworow {
 
 	private JFrame frame;
 	private JTextField tytu;
 	private JTextField wyko;
-	private JTextField data_publi;
 	private JTextField albu;
+	private JTextField data_publi;
 
 	/**
 	 * Launch the application.
@@ -31,7 +30,7 @@ public class dodawnie_utworow {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					dodawnie_utworow window = new dodawnie_utworow();
+					usuwanie_utworow window = new usuwanie_utworow();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,7 +42,7 @@ public class dodawnie_utworow {
 	/**
 	 * Create the application.
 	 */
-	public dodawnie_utworow() {
+	public usuwanie_utworow() {
 		initialize();
 	}
 
@@ -52,71 +51,75 @@ public class dodawnie_utworow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 680, 190);
+		frame.setBounds(100, 100, 680, 180);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblDodawanieNowegoUtworu = new JLabel("Dodawanie nowego utworu");
-		lblDodawanieNowegoUtworu.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		lblDodawanieNowegoUtworu.setBounds(25, 11, 275, 33);
-		frame.getContentPane().add(lblDodawanieNowegoUtworu);
-		
-		JLabel tyt = new JLabel("Tytu\u0142");
-		tyt.setBounds(25, 82, 86, 14);
-		frame.getContentPane().add(tyt);
-		
-		JLabel wyk = new JLabel("Wykonawca");
-		wyk.setBounds(150, 82, 86, 14);
-		frame.getContentPane().add(wyk);
-		
-		JLabel alb = new JLabel("Album");
-		alb.setBounds(281, 82, 86, 14);
-		frame.getContentPane().add(alb);
-		
-		JLabel data_pub = new JLabel("Data_publikacji");
-		data_pub.setBounds(401, 82, 101, 14);
-		frame.getContentPane().add(data_pub);
+		JLabel lblUsuwanieUtworu = new JLabel("Usuwanie uworu");
+		lblUsuwanieUtworu.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblUsuwanieUtworu.setBounds(25, 11, 260, 51);
+		frame.getContentPane().add(lblUsuwanieUtworu);
 		
 		tytu = new JTextField();
-		tytu.setBounds(25, 107, 86, 20);
+		tytu.setBounds(25, 94, 86, 20);
 		frame.getContentPane().add(tytu);
 		tytu.setColumns(10);
 		
 		wyko = new JTextField();
-		wyko.setBounds(150, 107, 86, 20);
+		wyko.setBounds(163, 94, 86, 20);
 		frame.getContentPane().add(wyko);
 		wyko.setColumns(10);
 		
 		albu = new JTextField();
-		albu.setColumns(10);
-		albu.setBounds(281, 107, 86, 20);
+		albu.setBounds(302, 94, 86, 20);
 		frame.getContentPane().add(albu);
+		albu.setColumns(10);
 		
 		data_publi = new JTextField();
-		data_publi.setColumns(10);
-		data_publi.setBounds(401, 107, 86, 20);
+		data_publi.setBounds(440, 94, 86, 20);
 		frame.getContentPane().add(data_publi);
+		data_publi.setColumns(10);
 		
-		JButton btnDodaj = new JButton("dodaj");
-		btnDodaj.addActionListener(new ActionListener() {
+		JButton btnUsun = new JButton("usun");
+		btnUsun.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					Class.forName("com.mysql.cj.jdbc.Driver");
 					Connection con=DriverManager.getConnection("jdbc:mysql://localhost/wypozyczalnia","root","");
 					Statement stmt=con.createStatement();
-					String sql="INSERT INTO utwory (Tytul, Wykonawca, Album, Data_publikacji) "
-							+ "VALUES('"+tytu.getText()+"','"+wyko.getText()+"','"+albu.getText()+"','"+data_publi.getText()+"')";
+					String sql="DELETE FROM utwory WHERE (Tytul, Wykonawca, Album, Data_publikacji) ="
+							+ " ('"+tytu.getText()+"','"+wyko.getText()+"','"+albu.getText()+"','"+data_publi.getText()+"')";
+							
 					int rs=stmt.executeUpdate(sql);
 					if(rs==1)
-						JOptionPane.showMessageDialog(null, "Utwór został dodany");
+						JOptionPane.showMessageDialog(null, "utwor zostal usuniety");
 					else 
-						JOptionPane.showMessageDialog(null, "B³êdny login lub haslo");
+						JOptionPane.showMessageDialog(null, "Bledne dane");
 					con.close();
-				} catch(Exception e1){System.out.println(e1);}
+				} catch(Exception e1)
+				{
+					System.out.println(e1);
+					}
 			}
 		});
 		
-		btnDodaj.setBounds(533, 106, 89, 23);
-		frame.getContentPane().add(btnDodaj);
+		btnUsun.setBounds(550, 93, 89, 23);
+		frame.getContentPane().add(btnUsun);
+		
+		JLabel tyt = new JLabel("Tytu\u0142");
+		tyt.setBounds(25, 73, 86, 14);
+		frame.getContentPane().add(tyt);
+		
+		JLabel wyk = new JLabel("Wykonawca");
+		wyk.setBounds(163, 73, 86, 14);
+		frame.getContentPane().add(wyk);
+		
+		JLabel alb = new JLabel("Album");
+		alb.setBounds(302, 73, 86, 14);
+		frame.getContentPane().add(alb);
+		
+		JLabel dat_pub = new JLabel("Data publikacji");
+		dat_pub.setBounds(440, 73, 99, 14);
+		frame.getContentPane().add(dat_pub);
 	}
 }
