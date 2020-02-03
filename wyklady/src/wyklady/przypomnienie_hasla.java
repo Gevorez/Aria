b54bc8e8beb49668b7b8c19cbdf1940107121666
Dominik Rybicki <dominpl13@gmail.com>
2020-02-03 10:45:08 +0100

    Na razie wrzucam bez SHA1 ale postaram sie to ogarnac

--- wyklady/src/wyklady/przypomnienie_hasla.java
+++ wyklady/src/wyklady/przypomnienie_hasla.java
@@ -1,12 +1,14 @@
 package wyklady;
 
 import java.awt.EventQueue;
-
+import java.math.BigInteger; 
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
+import wyklady.szyfrowanie;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -15,11 +17,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.awt.event.ActionEvent;
-
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import java.security.MessageDigest;
 public class przypomnienie_hasla {
 
 	private JFrame frame;
 	private JTextField pomoc;
+	private JPasswordField password;
 
 	/**
 	 * Launch the application.
@@ -49,21 +55,74 @@ public class przypomnienie_hasla {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 621, 383);
+		frame.setBounds(100, 100, 485, 291);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		pomoc = new JTextField();
-		pomoc.setBounds(54, 67, 296, 19);
+		pomoc.setBounds(89, 83, 296, 19);
 		frame.getContentPane().add(pomoc);
 		pomoc.setColumns(10);
 		
-		JLabel lblPrzypomnienieHasla = new JLabel("Podaj odpowiedz na pytanie pomocnicze");
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(54, 259, 418, -252);
+		frame.getContentPane().add(layeredPane);
+		layeredPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, -231, 347, 179);
+		layeredPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Wpisz nowe haslo");
+		lblNewLabel.setBounds(10, 11, 276, 26);
+		panel.add(lblNewLabel);
+		
+		JButton zmianka = new JButton("Zmien");
+		zmianka.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					Connection con=DriverManager.getConnection("jdbc:mysql://localhost/wypozyczalnia","root","");
+					Statement stmt=con.createStatement();
+					
+					
+						String sql="Update login "
+								+ "set Haslo='"+password.getText()+"'"
+								+"where pytanie_pomoc='"+pomoc.getText()+"'";
+						
+
+						;
+						
+						
+						int rs=stmt.executeUpdate(sql);
+						if(rs==1) {
+							JOptionPane.showMessageDialog(null, "haslo zmienione");
+							password.setText(null);
+						}
+						
+					
+					con.close();
+				} catch(Exception e1){System.out.println(e1);}
+			
+				
+			}
+		});
+		zmianka.setBounds(93, 94, 89, 23);
+		panel.add(zmianka);
+		
+		password = new JPasswordField();
+		password.setBounds(64, 62, 185, 20);
+		panel.add(password);
+		
+		
+		JLabel lblPrzypomnienieHasla = new JLabel("Podaj odpowiedz na pytanie pomocnicze aby zmienic haslo");
+		lblPrzypomnienieHasla.setBounds(10, 0, 476, 102);
 		lblPrzypomnienieHasla.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblPrzypomnienieHasla.setBounds(54, 24, 355, 33);
 		frame.getContentPane().add(lblPrzypomnienieHasla);
 		
-		JButton btnPrzypomnij = new JButton("Przypomnij");
+		JButton btnPrzypomnij = new JButton("Zmien");
+		btnPrzypomnij.setBounds(175, 117, 121, 21);
 		btnPrzypomnij.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -73,23 +132,30 @@ public class przypomnienie_hasla {
 						Statement stmt=con.createStatement();
 						String test=pomoc.getText();
 						
-						String sql3="Select Haslo, pytanie_pomoc from login where pytanie_pomoc ='"+test+"'";
+						String sql3="Select  pytanie_pomoc from login where pytanie_pomoc ='"+test+"'";
 						ResultSet rs3=stmt.executeQuery(sql3);
 						rs3.next();
 						String pomoc = rs3.getString("pytanie_pomoc");
-						String haslo = rs3.getString("Haslo");
+						
 						
 							
 						if(pomoc != test)
-							JOptionPane.showMessageDialog(null, "Twoje has³o to: "+ haslo);
-						
+						{
+					    layeredPane.removeAll();
+						layeredPane.add(panel);
+						layeredPane.repaint();
+						layeredPane.revalidate();
+						panel.setVisible(true);
+						frame.setContentPane(panel);
+						}
 						
 						con.close();
 						}
 					 catch(Exception e1){JOptionPane.showMessageDialog(null, "B³êdna odpowiedz");}
 			}
 		});
-		btnPrzypomnij.setBounds(265, 96, 85, 21);
 		frame.getContentPane().add(btnPrzypomnij);
+		
+		
 	}
 }
